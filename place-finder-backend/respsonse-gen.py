45e3687b350d64f9e2745c5cb60be35aefc5daad
HaiderMalikk <haidermalik662@gmail.com>
2024-12-18 17:12:50 -0500

    small update and note

--- place-finder-backend/respsonse-gen.py
+++ place-finder-backend/respsonse-gen.py
@@ -2,3 +2,9 @@
 // main file
 
 """
+from langchain.chat_models import ChatOpenAI as AI
+
+resp = AI.print(AI.model(model = "gpt-3.5-turbo", key= OPENAIKEY))
+
+# !!! Cd into this folder to install dependencies
+

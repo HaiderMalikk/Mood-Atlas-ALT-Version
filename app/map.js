09e5e2f0d1dbdc3f1492c18a0b073d53f4d2e27b
HaiderMalikk <haidermalik662@gmail.com>
2024-12-20 19:40:37 -0500

    Custom google map style in

--- app/map.js
+++ app/map.js
@@ -20,12 +20,12 @@ export function Map() {
             })
             const {Map} = await loader.importLibrary('maps'); // load the map library
             const position = { lat: 43.642693, lng: -79.3871189 }; // position of the map (center)
-            const googlemapsoptions = { center: position, zoom: 12, mapTypeId: 'roadmap' }; // map options
+            const googlemapsoptions = { center: position, zoom: 14, mapId: process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID }; // map options
 
             // setup map
             const map = new Map(mapRef.current, googlemapsoptions);
             
-            // setup marker
+            //setup marker
             const { Marker } = await loader.importLibrary('marker'); // load the marker library
             // put the marker on the map
             const marker = new Marker({
